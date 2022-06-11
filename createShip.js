84f72124954340ca270d8dedef9692dce35e6478
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-06-11 15:26:57 +0500

    Create ship factory

---
+++ createShip.js
@@ -0,0 +1,50 @@
+function createShip(origin, length, position = 'horizontal') {
+  const shipBlocks = [];
+
+  const createShipBlock = (coord) => ({
+    coords: {
+      x: coord.x,
+      y: coord.y
+    },
+    hit: false
+  })
+  
+  const addOffset = (() => {
+    if (position == 'horizontal') 
+    return (offset) => ({
+      x: origin.x + offset,
+      y: origin.y    
+    })
+    
+    else if (position == 'vertical') 
+    return (offset) => ({
+      x: origin.x,
+      y: origin.y + offset    
+    })
+  })();
+  
+  for (let offset = 0; offset < length; offset++) {
+    const shipBlock = createShipBlock(addOffset(offset))
+    shipBlocks.push(shipBlock)
+  }
+
+
+  const hit = (coords) => {
+    const target = shipBlocks.find((block) => {
+      return (block.coords.x == coords.x 
+           && block.coords.y == coords.y)
+    });
+
+    if (target) {
+      target.hit = true;
+      return true
+    }
+    return false
+  };
+
+  const isSunk = () => shipBlocks.every(block => block.hit == true);
+
+  return { hit, isSunk }
+}
+
+module.exports = createShip
