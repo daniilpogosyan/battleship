e999cf59ca16a5d7fbd0fafdbbb925fa2d2dbd74
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-06-15 16:27:00 +0500

    Refactor gameboard methods

--- modules/createGameboard/createGameboard.js
+++ modules/createGameboard/createGameboard.js
@@ -1,20 +1,35 @@
 function createGameboard () {
   const grid = Array.from(Array(10), () => Array(10).fill(null));
+  const fleet = [];
 
   const getGrid = () => {
     return Array.from(grid, (arr1D) => [...arr1D])
   }
 
+  const coordsExist = (coords) => (
+    coords.x < 10 && coords.x >= 0
+    && coords.y < 10 && coords.y >= 0
+  )
+
+  const getAdjacentCells = (coords) => {
+    const adjacentCoords = [];
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        const currentCoords = {x: coords.x + i, y: coords.y + j};
+        if (coordsExist(currentCoords) && (i !==0 || j !== 0))
+            adjacentCoords.push({x: coords.x + i, y: coords.y + j});
+      }
+    }
+    return adjacentCoords;
+  }
   
   const cellAvailable = (coords) => {
     if (grid[coords.x]?.[coords.y] !== null) return false
 
-    for (let i = -1; i <= 1; i++) {
-      for (let j = -1; j <= 1; j++) {
-        if (grid[coords.x + i]?.[coords.y + j] !== undefined
-            && grid[coords.x + i]?.[coords.y + j] !== null)
+    const adjacentCoords = getAdjacentCells(coords);
+    for (let coords of adjacentCoords) {
+        if (grid[coords.x]?.[coords.y] === 'ship')
           return false 
-      }
     }
 
     return true
@@ -35,17 +50,20 @@ function createGameboard () {
       }
     })();
 
-    const wouldbeShipBlocks = [];
+    const wouldbeShipCoords = [];
     for (let offset = 0; offset < ship.length; offset++) {
       const cell = addOffset(offset);
       if (!cellAvailable(cell)) return false
-      wouldbeShipBlocks.push(cell);
+      wouldbeShipCoords.push(cell);
     }
 
-    wouldbeShipBlocks.forEach(block => grid[block.x][block.y] = {
-      ship: ship,
-      origin: origin
-    })
+    wouldbeShipCoords.forEach(coords => grid[coords.x][coords.y] = 'ship');
+
+    fleet.push({
+      coords: wouldbeShipCoords,
+      ship: ship
+    });
+
     return true
   }
 
